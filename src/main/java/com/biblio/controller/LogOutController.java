dbf4b2fedec675d2e9dcd796fb6dc8b6f18c0618
LeTanTru <letantru@gmail.com>
2025-05-04 21:53:07 +0700

    update EmailServiceImpl && update LogOutController

--- src/main/java/com/biblio/controller/LogOutController.java
+++ src/main/java/com/biblio/controller/LogOutController.java
@@ -6,38 +6,29 @@ import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.Serial;
 
-/**
- * Servlet implementation class LogOutController
- */
 @WebServlet("/logout")
 public class LogOutController extends HttpServlet {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public LogOutController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        HttpSession session = request.getSession();
-        if (session.getAttribute("account") != null) {
-            session.removeAttribute("account");
+        HttpSession session = request.getSession(false);
+        if (session != null) {
             session.invalidate();
         }
 
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                if ("username".equals(cookie.getName())) {
+                if ("username".equals(cookie.getName()) || "JSESSIONID".equals(cookie.getName())) {
+                    cookie.setValue("");
                     cookie.setMaxAge(0);
+                    cookie.setPath("/");
                     response.addCookie(cookie);
                 }
             }
@@ -46,12 +37,8 @@ public class LogOutController extends HttpServlet {
         response.sendRedirect(request.getContextPath() + "/login");
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         doGet(request, response);
     }
-
 }
