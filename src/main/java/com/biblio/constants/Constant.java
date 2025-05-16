d43d2cbe3fceb01a61912718b2d19fafa5068617
huypham <huyphuocbinh204@gmail.com>
2025-05-16 13:44:46 +0700

    UTESDTE-8: change config

--- src/main/java/com/biblio/constants/Constant.java
+++ src/main/java/com/biblio/constants/Constant.java
@@ -22,7 +22,7 @@ public class Constant {
     private static final String DB_NAME = "bsw-biblio";
     public static final String DB_URL = "jdbc:mysql://" + HOST_NAME + ":" + PORT + "/" + DB_NAME;
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "root";
 
     public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String DB_MIN_CONNECTIONS = "5";
